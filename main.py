390cae63a83d3288102265fc68a3ebb10a57408e
Shelby Easley <shelbyeasley3@gmail.com>
2017-10-04 20:34:14 -0500

    getting closer

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/welcome", methods=["POST"])
+@app.route("/", methods=["POST"])
 def welcome():
-    if username == "" or username < 3 or username > 20:
+    username = request.form['username']
+    if username == "" or len(username) < 3 or len(username) > 20:
         error = "username is not valid"
         return redirect("/?error=" + error)
-    if password == "" or password < 3 or username > 20:
+    password = request.form['password']
+    if password == "" or len(password) < 3 or len(password) > 20:
         error = "password is not valid"
         return redirect("/?error=" + error)
+    verify = request.form['verify']
     if password != verify:
         error = "passwords do not match"
         return redirect("/?error=" + error)
@@ -22,7 +25,7 @@ def welcome():
 @app.route("/")
 def index():
     error = request.args.get("error")
-    return render_template('index.html', error=error and cgi.escape(error, quote=true))
+    return render_template('index.html', error=error and cgi.escape(error, quote=True))
 
 if __name__ == "__main__":
     app.run()
