b83174c6e2e47303a02cd36fe4d9e613065d9d33
Shelby Easley <shelbyeasley3@gmail.com>
2017-10-05 19:49:54 -0500

    done

--- main.py
+++ main.py
@@ -7,20 +7,37 @@ app.config['DEBUG'] = True
 
 @app.route("/", methods=["POST"])
 def welcome():
+    error1 = ""
+    error2 = ""
+    error3 = ""
+    error4 = ""
     username = request.form['username']
-    if username == "" or len(username) < 3 or len(username) > 20:
-        error = "username is not valid"
-        return redirect("/?error=" + error)
+    if username == "" or len(username) < 3 or len(username) > 20 or " " in username:
+        error1 = "username is not valid"
+    
     password = request.form['password']
-    if password == "" or len(password) < 3 or len(password) > 20:
-        error = "password is not valid"
-        return redirect("/?error=" + error)
+    if password == "" or len(password) < 3 or len(password) > 20 or " " in username:
+        error2 = "password is not valid"
+        
     verify = request.form['verify']
-    if password != verify:
-        error = "passwords do not match"
-        return redirect("/?error=" + error)
-
-    return render_template('welcome.html', username=username)
+    if verify == "":
+        error3 = "must verify password"
+        
+    if verify != password:
+        error3 = "passwords do not match"
+        
+    email = request.form['email']
+    if "@" not in email or "." not in email or " " in email or len(email) < 3 or len(email) > 20:
+        error4 = "not a valid email address"
+    
+    if email == "":
+        error4 = ""
+        
+    if error1 or error2 or error3 or error4:
+        return render_template('index.html', username=username, password=password, error1=error1, error2=error2, error3=error3, error4=error4)
+    
+    else:
+        return render_template('welcome.html', username=username)
 
 @app.route("/")
 def index():
